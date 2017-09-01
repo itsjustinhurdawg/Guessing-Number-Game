cac39dc3066d4e5a36c598799f17f2bf7f9ec756
itsjustinhurdawg <30875159+itsjustinhurdawg@users.noreply.github.com>
2017-09-01 08:41:42 -1000

    Create guessingGame.js

---
+++ guessingGame.js
@@ -0,0 +1,14 @@
+var playerOne = prompt("Enter a number between 1-6");
+if ((playerOne < 1) || (playerOne > 6 ) || isNaN(playerOne)) {
+window.alert("You cannot follow directions");
+}
+else {
+var playerTwo = prompt ("Guess a number between 1-6");
+if (playerOne == playerTwo) {
+window.alert("You Win!");
+}
+else {
+window.alert("Player one answer was " + playerOne);
+window.alert("You chose " + playerTwo);
+}
+}
